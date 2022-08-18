54057d5614508a826f5a6b57654d7ac6d909dcd7
Yhomarth Reyes <yhomarth@gmail.com>
2022-08-18 15:45:01 -0400

    Crear usuario en base de datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,50 @@
 const { request, response } = require('express');
-const { validationResult } = require('express-validator')
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req = request, res = response )=> {
+const crearUsuario = async (req = request, res = response )=> {
     
     const { name, email, password } = req.body;
 
-    return res.json({
-        ok : true,
-        msg : 'Crear usuarios /new'
-    });
+    try {
+        // Verificar el Email
+        const usuario = await Usuario.findOne({ email });
+
+        if(usuario) {
+            return res.status(400).json({
+                ok : false,
+                msg : 'Ya existe un usuario con este email'
+            });
+        }
+
+        // Crear usuario con el modelo
+        const dbUser = new Usuario( req.body );
+
+        // Hash la contraseña
+
+        // Generar el JWT
+
+
+        // crear Usuario de base de datos
+        await dbUser.save();
+
+
+
+        // Generar respuesta exitosa
+        return res.status(201).json({
+            ok : true,
+            uid : dbUser.id,
+            name
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok : false,
+            msg : 'Error ACDB - comunicar con el administrador'
+        });
+    }
+
+    
 };
 
 
