4df2a9ae9cc44e42602de1c58a57572e6e55e7b9
Yhomarth Reyes <yhomarth@gmail.com>
2022-08-18 14:27:06 -0400

    Custom Middleware - ValidarCampos

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,10 +2,8 @@ const { request, response } = require('express');
 const { validationResult } = require('express-validator')
 
 const crearUsuario = (req = request, res = response )=> {
-
+    
     const { name, email, password } = req.body;
-
-    console.log( name, email, password );
 
     return res.json({
         ok : true,
@@ -16,18 +14,7 @@ const crearUsuario = (req = request, res = response )=> {
 
 const loginUsuario = (req = request, res = response)=> {
 
-    const errors = validationResult( req );
-    
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok : false,
-            mag : errors.mapped()
-        })
-    }
-
    const { email, password } = req.body;
-
-    console.log( email, password );
 
     return res.json({
         ok : true,
--- routes/auth.js
+++ routes/auth.js
@@ -2,20 +2,26 @@
 const { Router } = require('express');
 const { check } = require('express-validator')
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 
 // crear un nuevo usuario
-router.post('/new', crearUsuario);
+router.post('/new', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'La contraseña no cumple con la longitud').isLength({min : 6}),
+    validarCampos
+],crearUsuario);
 
 
 // Login de usuario
 router.post('/', [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'La contraseña no cumple con la longitud').isLength({min : 6}),
-]
-,loginUsuario);
+    validarCampos
+],loginUsuario);
 
 
 
